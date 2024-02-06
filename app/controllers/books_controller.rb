261cbabb0ff18421864ae51235249b4af58ef82f
150042192+docdog158@users.noreply.github.com <ec2-user@ip-172-31-31-106.ap-northeast-1.compute.internal>
2024-02-06 15:14:08 +0000

    一旦保存

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,20 +8,17 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      
-      redirect_to book_path(@book.id)
       flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
-      
       @books =Book.all
       render :index
-      
     end
   end
   
 
   def show
-    @book = Book.find(params[:id]) 
+    @book = Book.find(params[:id])
   end 
 
   def edit
@@ -31,17 +28,12 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      
-      redirect_to book_path(@book.id) 
       flash[:notice] = "Book was successfully updated."
-      
+      redirect_to book_path(@book.id) 
     else
-      
       render :edit
     end
   end
-  
-
   
   def destroy
     @book = Book.find(params[:id])  
